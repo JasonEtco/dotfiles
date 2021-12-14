2528ad4e95b2e46b8f2d26500550401f3cc2fb5f
Jason Etcovitch <jasonetco@github.com>
2021-12-14 14:43:38 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,15 @@
 set fish_greeting
 set PATH /usr/local/bin $PATH
-set PATH /home/linuxbrew/.linuxbrew/bin $PATH
 
 alias reload "exec fish"
 
 alias git "hub"
 alias gc "git checkout"
+alias gl "git log -1 --format='%H'"
 
-alias ghs='GH_SSL=1 ENABLE_EMAIL_PREVIEWS=1 script/server'
-alias ghd='GH_SSL=1 ENABLE_EMAIL_PREVIEWS=1 script/server --debug'
-alias ghr='launchctl stop github.dev.launch_socket_server_ssl && launchctl remove github.dev.launch_socket_server_ssl && brew services restart nginx'
+# GitHub specific
+alias ghs='script/server'
+alias ghd='script/server --debug'
 
 # Setup Starship
 starship init fish | source
